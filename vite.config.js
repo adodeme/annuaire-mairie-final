41e4b7c2c3b2ca0a4afe0d879110f9ffb37de13c
adodeme <adodemeflodias@gmail.com>
2026-06-05 12:59:26 +0100

    fix render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/annuaire-mairie-final/',
   plugins: [
     react(),
     tailwindcss(),
